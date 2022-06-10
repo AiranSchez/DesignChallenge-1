4ca0c5efd0dbc87b51579a5f3942db328ae1ec5d
airan <airan.sanchez-ext@clarity.ai>
2022-06-10 14:02:52 +0100

    :art: add background gradient

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
 <style>
 @import './assets/base.css';
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap');
-  
+
 :root{
     --white: #f9f9f9;
     --black: #36383F;
@@ -32,7 +32,8 @@ export default {
 }
 
 body {
-  background-color: var(--white);
+  background: rgb(41,64,73);
+  background: linear-gradient(90deg, rgba(41,64,73,1) 0%, rgba(0,0,0,1) 100%);
   font-family: "Poppins", sans-serif;
 }
 
