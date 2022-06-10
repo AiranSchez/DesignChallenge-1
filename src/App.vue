3a720abb47793a2f93d2b0ec696c965e063f7334
airan <airan.sanchez-ext@clarity.ai>
2022-06-10 13:15:34 +0100

    :art: extract header to external a component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,28 @@
-<script setup lang="ts">
+<script>
+import Header from './components/Header.vue'
 
+export default {
+  name: 'app',
+  components: {
+    Header
+  }
+};
 </script>
 
 <template>
-  <header>
-    <div id="title">Xae</div>
-      <input class="side-menu" type="checkbox" id="side-menu"/>
-      <!-- Hamburger icon -->
-      <label class="hamb" for="side-menu">
-        <span class="hamb-line"></span>
-      </label>
-      <!-- Menu -->
-      <nav class="nav">
-          <ul class="menu">
-              <li><a href="#">Gallery</a></li>
-              <li><a href="#">Blog</a> </li>
-              <li><a href="#">About</a></li>
-          </ul>
-      </nav>
-  </header>
+  <Header />
+  <main></main>
 </template>
 
 <style>
 @import './assets/base.css';
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap');
+  
+:root{
+    --white: #f9f9f9;
+    --black: #36383F;
+    --gray: #85888C;
+} /* variables*/
 
 * {
   margin: 0;
@@ -33,54 +32,8 @@
 }
 
 body {
+  background-color: var(--white);
   font-family: "Poppins", sans-serif;
-}
-
-header{
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  height: 62px;
-}
-
-#title{
-  font-size: 2em;
-  color: black;
-  font-weight: bold;
-}
-
-header div{
-  display: flex;
-}
-
-.hamburguer{
-  background-color: transparent;
-  border: none;
-  outline: none;
-  cursor: pointer;
-}
-
-.hamburguer .line{
-  display: block;
-  width: 40px;
-  height: 3px;
-  background-color: #ecf0f1;
-  margin-block: 10px;
-  border-radius: 4px;
-  transition: transform .5s, opacity .25s;
-}
-
-.hamburguer.active .line:nth-child(1){
-  transform: translateY(13px) rotate(45deg);
-}
-
-.hamburguer.active .line:nth-child(2){
-  opacity: 0;
-}
-
-.hamburguer.active .line:nth-child(3){
-  transform: translateY(-13px) rotate(-45deg);
 }
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,118 @@
+<script>
+  export default {};
+</script>
+
+
+<template>
+    <header>
+        <!-- Logo -->
+        <a href="#" class="logo">Xae</a>
+        <!-- Hamburger icon -->
+        <input class="side-menu" type="checkbox" id="side-menu"/>
+        <label class="hamb" for="side-menu">
+          <span class="hamb-line"></span>
+        </label>
+        <!-- Menu -->
+        <nav class="nav">
+            <ul class="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Gallery</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style>
+a{
+    text-decoration: none;
+}
+
+ul{
+    list-style: none;
+}
+
+header{
+  position: sticky;
+  top: 0;
+  height: 62px;
+}
+
+.logo{
+  font-size: 4em;
+  margin-left: 0.1em;
+  color: black;
+  font-weight: bold;
+}
+
+header div{
+  display: flex;
+}
+
+.nav{
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    overflow: hidden;
+}
+.menu a{
+    display: flex;
+    padding: 2em;
+}
+.menu a:hover{
+    background-color: var(--gray);
+}
+.nav{
+    max-height: 0;
+    transition: max-height .5s ease-out;
+}
+.hamb{
+    cursor: pointer;
+    float: right;
+    padding: 40px 20px;
+}
+.hamb-line {
+    display: block;
+    background: var(--black);
+    height: 2px;
+    position: relative;
+    width: 24px;
+
+} 
+.hamb-line::before,
+.hamb-line::after{
+    background: var(--black);
+    content: '';
+    display: block;
+    height: 100%;
+    position: absolute;
+    transition: all .2s ease-out;
+    width: 100%;
+}
+.hamb-line::before{
+    top: 5px;
+}
+.hamb-line::after{
+    top: -5px;
+}
+
+.side-menu {
+    display: none;
+}
+.side-menu:checked ~ nav{
+    max-height: 100%;
+}
+.side-menu:checked ~ .hamb .hamb-line {
+    background: transparent;
+}
+.side-menu:checked ~ .hamb .hamb-line::before {
+    transform: rotate(-45deg);
+    top:0;
+}
+.side-menu:checked ~ .hamb .hamb-line::after {
+    transform: rotate(45deg);
+    top:0;
+}
+
+</style>
