7a885fb329b58dc29a6056a837464bf5e7f22c4e
airan <airan.sanchez-ext@clarity.ai>
2022-06-10 14:02:22 +0100

    :art: refactor to more components

---
+++ src/components/Logo.vue
@@ -0,0 +1,15 @@
+<script>
+  export default {};
+</script>
+
+<template>
+    <a href="#" class="logo">Xae</a>
+</template>
+
+<style>
+.logo{
+  font-size: 4em;
+  margin-left: 0.1em;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,40 @@
+<script>
+  export default {};
+</script>
+
+<template>
+  <nav class="nav">
+    <ul class="menu">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Gallery</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+.nav{
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    overflow: hidden;
+}
+.menu a{
+    display: flex;
+    justify-content: center;
+    padding: 2em;
+}
+.menu li a {
+  color: var(--white);
+  font-size: 1em;
+  font-weight: bold;
+}
+.menu a:hover{
+    background-color: var(--gray);
+}
+.nav{
+    max-height: 0;
+    transition: max-height .5s ease-out;
+}
+</style>
